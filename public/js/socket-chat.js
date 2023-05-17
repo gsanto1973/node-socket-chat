6b39e43bbccf017a8c08df004f42cb50456541ce
gsanto1973 <93636042+gsanto1973@users.noreply.github.com>
2023-05-16 21:07:54 -0500

    Chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -15,15 +15,19 @@ var usuario = {
 
 // Escuchar información
 socket.on('connect', function () {
+
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function (resp) {
-        console.log('Usuarios conectados', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
 });
 
 
 // Escuchar información
 socket.on('disconnect', function () {
+
     console.log('Perdimos conexión con el servidor');
 });
 
@@ -38,18 +42,27 @@ socket.on('disconnect', function () {
     console.log('respuesta server: ', resp);
 }); */
 
+
 // Escuchar información
 socket.on('crearMensaje', function (mensaje) {
-    console.log('Servidor:', mensaje);
+
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
+
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function (personas) {
+
     console.log(personas);
+    renderizarUsuarios(personas);
 });
+
 
 // Mensajes privados
 socket.on('mensajePrivado', function (mensaje) {
+
     console.log('Mensaje privado:', mensaje);
 })
